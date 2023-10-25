6d4ac7059febb4b2e696cefcccd5e3e89a38e6fb
sanjana9560 <121418058+sanjana9560@users.noreply.github.com>
2023-10-25 05:48:15 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+      numb: 1,
+      question: "Which of the following attributes is used to add link to any element?",
+      answer: "href",
+      options: [
+        "link",
+        "href",
+        "ref",
+        "newref"
+      ],
+      log:""
+    },
+      {
+      numb: 2,
+      question: "What does HTML stand for?",
+      answer: "Hyper Text Markup Language",
+      options: [
+        "Hyper Text Markup Language",
+        "Hyper Text Preprocessor",
+        "Hyper Text Multiple Language",
+        "Hyper Tool Multi Language"
+      ],
+      logo:"./html.png"
+    },
+    {
+      numb: 3,
+      question: "What does PHP stand for?",
+      answer: "Hypertext Preprocessor",
+      options: [
+        "Hypertext Preprocessor",
+        "Hypertext Programming",
+        "Hypertext Preprogramming",
+        "Hometext Preprocessor"
+      ],
+      logo:"./php.png"
+    },
+      {
+      numb: 4,
+      question: "What does CSS stand for?",
+      answer: "Cascading Style Sheet",
+      options: [
+        "Common Style Sheet",
+        "Colorful Style Sheet",
+        "Computer Style Sheet",
+        "Cascading Style Sheet"
+      ],
+      logo:"./css.png"
+    },
+      
+      
+  ];
+
